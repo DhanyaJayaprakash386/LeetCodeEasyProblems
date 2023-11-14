17cab0c2c1f023a41fa33403c81e88d8c244acde
unknown <dhanyajaya1234@gmail.com>
2023-11-14 11:57:31 +0530

    Two Sums sol in C

--- TWO SUMS/SolinC.c
+++ TWO SUMS/SolinC.c
@@ -1,10 +1,27 @@
 #include<stdio.h>
+int *TwoSums(int *nums,int Numsize,int target){
+    int sum = 0 ;
+    int i,j;
+    static int result[2]={0,0};
+    for(i=0;i<Numsize-2;i++){
+        for(j=i+1;j<=Numsize-1;j++){
+            sum = nums[i]+nums[j];
+            if(target == sum){
+                result[0] = i;
+                result[1] = j;
+            }
+        }
+    }
+    return result;
+}
+void main(){
 
-int main(){
-
-    printf("Hello World!!!");
+    int nums[] = {2,7,11,15,8};
+    int target =15;
+    int length = sizeof(nums)/sizeof(nums[0]);
+    int *arr;
+    arr =TwoSums(nums,length,target);
+    printf("[%d,%d]",arr[0],arr[1]);
+    
 }
 
-/*int* twoSum(int* nums, int numsSize, int target){
-    return 1;
-}*/
